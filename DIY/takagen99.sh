5f6058fb7020cfe9651914eec6a5735db9246420
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-11-07 15:48:19 +0800

    更新 takagen99.sh

--- DIY/takagen99.sh
+++ DIY/takagen99.sh
@@ -3,6 +3,7 @@
 cp -rf $GITHUB_WORKSPACE/DIY/png/图标9-1.png app/src/main/res/drawable/app_banner.png
 cp -rf $GITHUB_WORKSPACE/DIY/png/图标9.png app/src/main/res/drawable/app_icon.png
 cp -rf $GITHUB_WORKSPACE/DIY/png/app_bg.png app/src/main/res/drawable/app_bg.png
+sed -i 's%com.github.tvbox.osc.tk%com.github.tvbox.osc.${{ matrix.osName }}%g' app/build.gradle
 sed -i 's%TVBox%大师影视%g' app/src/main/res/values-zh/strings.xml
 sed -i 's%>https://chengxue2020.github.io/Cat-ports/pg/jsm.json<%>https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json<% g' app/src/main/res/values-zh/strings.xml
 
