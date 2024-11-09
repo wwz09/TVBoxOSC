43374dd0f3875b13d1b7c640e9443b5852fb301a
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-11-09 23:00:27 +0800

    Add files via upload

--- DIY/dc/App.java
+++ DIY/dc/App.java
@@ -192,14 +192,12 @@ public class App extends MultiDexApplication {
         // 默认线路地址
         String defaultApiName = "默认线路";
         String defaultApi = "https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json";
-       // 默认仓库地址
-        String defaultStoreApi = "https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT//dc.json";
-
-        Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());
-        defaultApiMap.put(defaultApiName, defaultApi);
-
-        List<String> defaultApiHistory = Hawk.get(HawkConfig.API_NAME_HISTORY, new ArrayList<>());
-        defaultApiHistory.add(defaultApiName);
+        // 默认仓库地址
+        // String defaultStoreApi = "https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT//dc.json";
+        // Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());
+        // defaultApiMap.put(defaultApiName, defaultApi);
+        // List<String> defaultApiHistory = Hawk.get(HawkConfig.API_NAME_HISTORY, new ArrayList<>());
+        // defaultApiHistory.add(defaultApiName);
         // putDefault(HawkConfig.API_URL, defaultApi);
         // putDefault(HawkConfig.API_NAME, defaultApiName);
         // putDefault(HawkConfig.API_NAME_HISTORY, defaultApiHistory);
