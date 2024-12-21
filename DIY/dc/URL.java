9245ab5c34a38129e50bb27273fed8b78b2f688b
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-12-21 22:29:58 +0800

    Update URL.java

--- DIY/dc/URL.java
+++ DIY/dc/URL.java
@@ -12,12 +12,12 @@ public class URL {
     // https://ghproxy.com/https://raw.githubusercontent.com//mlabalabala/TVResource/main/boxCfg/ori_source.json
     // https://raw.bunnylblbblbl.eu.org/https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/ori_source.json
     // 默认线路
-    public static final String DEFAULT_API_URL = "https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/mr.json";
+    public static final String DEFAULT_API_URL = "https://ghproxy.net/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/mr.json";
     // public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/gaotianliuyun/gao/master/0821.json";
     // 默认仓库线路
-    public static final String DEFAULT_STORE_API_URL = "https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json";
+    public static final String DEFAULT_STORE_API_URL = "https://ghproxy.net/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json";
     // 默认直播线路
-    public static final String DEFAULT_LIVE_URL = "https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt";
+    public static final String DEFAULT_LIVE_URL = "https://ghproxy.net/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt";
 
 
 }
