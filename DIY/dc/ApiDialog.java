d694ba59cda88c675ca60c4560ac6c17c1169a5d
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-12-21 22:31:51 +0800

    Update ApiDialog.java

--- DIY/dc/ApiDialog.java
+++ DIY/dc/ApiDialog.java
@@ -344,9 +344,9 @@ public class ApiDialog extends BaseDialog {
         URL.DOMAIN_NAME_PROXY = url;
         // 默认线路地址
         String defaultApiName = "默认线路";
-        String defaultApi = "https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json";
+        String defaultApi = "https://ghproxy.net/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json";
         // 默认仓库地址
-        String defaultStoreApi = "https://ghp.ci/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT//dc.json";
+        String defaultStoreApi = "https://ghproxy.net/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json";
 
         Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());
         defaultApiMap.put(defaultApiName, defaultApi);
