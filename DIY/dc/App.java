4d0ead86b11ac717ebc4066bddff02849880e2e6
wwz09 <47264059+wwz09@users.noreply.github.com>
2025-01-22 22:39:44 +0800

    更新 App.java

--- DIY/dc/App.java
+++ DIY/dc/App.java
@@ -122,11 +122,11 @@ public class App extends MultiDexApplication {
         Hawk.init(this).build();
 
         putDefault(HawkConfig.DEBUG_OPEN, false);
-        putDefault(HawkConfig.PLAY_TYPE, 1);
-        putDefault(HawkConfig.HOME_REC, 1);
+        putDefault(HawkConfig.PLAY_TYPE, 1);                // Player   0=系统, 1=IJK, 2=Exo
+        putDefault(HawkConfig.HOME_REC, 1);                 // Home Rec 0=豆瓣, 1=推荐, 2=历史
         // 默认渲染方式：推荐手机使用0-texture，电视1-surface
-        putDefault(HawkConfig.PLAY_RENDER, 1);
-        putDefault(HawkConfig.IJK_CODEC, "硬解码");
+        putDefault(HawkConfig.PLAY_RENDER, 1);              // 渲染方式 0=texture, 1=surface
+        putDefault(HawkConfig.IJK_CODEC, "硬解码");          // IJK Render 软解码, 硬解码
         putDefault(HawkConfig.HOME_REC_STYLE, false);// 首页多行
 
         putDefault(HawkConfig.PROXY_URL, URL.DOMAIN_NAME_PROXY);
