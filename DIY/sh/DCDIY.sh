2d0872a35e426e0beee4f3564bc0279011a0cb20
wwz09 <47264059+wwz09@users.noreply.github.com>
2025-01-22 22:21:45 +0800

    更新 DCDIY.sh

--- DIY/sh/DCDIY.sh
+++ DIY/sh/DCDIY.sh
@@ -8,6 +8,6 @@ sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main
 sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
 sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
-
+sed -i 's%硬解码%软解码%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 
 echo 'DIY end'
