fc21e7fd3487762af0346fd4f4952e34c0701ff6
wwz09 <wwz5124@gmail.com>
2025-04-29 18:19:50 +0800

    Update DCDIY.sh

--- DIY/sh/DCDIY.sh
+++ DIY/sh/DCDIY.sh
@@ -10,4 +10,4 @@ sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main
 sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%硬解码%软解码%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 
-echo 'DIY end
+echo 'DIY end'
