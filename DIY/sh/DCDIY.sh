fb2408aa9acccd0da6678ba5026925f56b0a1b01
wwz09 <wwz5124@gmail.com>
2025-10-03 11:37:20 +0800

    更新 DCDIY.sh

--- DIY/sh/DCDIY.sh
+++ DIY/sh/DCDIY.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #添加PY支持
 
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
-sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.top:8888/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/a.txt%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/TXT/dc.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
+sed -i 's%https://ghfast.top/https://raw.githubusercontent.com/wwz09/ubuntu/main/main.json%https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%硬解码%软解码%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 
 echo 'DIY end'
