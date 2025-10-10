342de36fd3d5506ae9fd3ad8ad95e4316b7e60f3
wwz09 <wwz5124@gmail.com>
2025-10-10 18:50:43 +0800

    sh

--- DIY/sh/zyangDC.sh
+++ DIY/sh/zyangDC.sh
@@ -9,13 +9,13 @@ cp -rf $GITHUB_WORKSPACE/DIY/png/图标0.png app/src/main/res/drawable-xxxhdpi/a
 cp -rf $GITHUB_WORKSPACE/DIY/png/等待图标.png app/src/main/res/drawable/icon_loading.png
 cp -rf $GITHUB_WORKSPACE/DIY/png/等待图标.png app/src/main/res/drawable-xhdpi/icon_loading.png
 cp -rf $GITHUB_WORKSPACE/DIY/dc/epg_data.json app/src/main/assets/epg_data.json
-cp -rf $GITHUB_WORKSPACE/DIY/dc/URL.java app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
-cp -rf $GITHUB_WORKSPACE/DIY/dc/ApiDialog.java app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-cp -rf $GITHUB_WORKSPACE/DIY/dc/PlayFragment.java app/src/main/java/com/github/tvbox/osc/bbox/ui/fragment/PlayFragment.java
-cp -rf $GITHUB_WORKSPACE/DIY/dc/PlayActivity.java app/src/main/java/com/github/tvbox/osc/bbox/ui/activity/PlayActivity.java
-cp -rf $GITHUB_WORKSPACE/DIY/dc/ModelSettingFragment.java app/src/main/java/com/github/tvbox/osc/bbox/ui/fragment/ModelSettingFragment.java
-cp -rf $GITHUB_WORKSPACE/DIY/dc/Checker.java app/src/main/java/com/github/tvbox/osc/bbox/util/Checker.java
-cp -rf $GITHUB_WORKSPACE/DIY/dc/App.java app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
+cp -rf $GITHUB_WORKSPACE/DIY/dc/URL1.java app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+# cp -rf $GITHUB_WORKSPACE/DIY/dc/ApiDialog.java app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+# cp -rf $GITHUB_WORKSPACE/DIY/dc/PlayFragment.java app/src/main/java/com/github/tvbox/osc/bbox/ui/fragment/PlayFragment.java
+# cp -rf $GITHUB_WORKSPACE/DIY/dc/PlayActivity.java app/src/main/java/com/github/tvbox/osc/bbox/ui/activity/PlayActivity.java
+# cp -rf $GITHUB_WORKSPACE/DIY/dc/ModelSettingFragment.java app/src/main/java/com/github/tvbox/osc/bbox/ui/fragment/ModelSettingFragment.java
+# cp -rf $GITHUB_WORKSPACE/DIY/dc/Checker.java app/src/main/java/com/github/tvbox/osc/bbox/util/Checker.java
+cp -rf $GITHUB_WORKSPACE/DIY/dc/AppA.java app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 sed -i 's%XXPermissions:13.6%XXPermissions:13.5%g' app/build.gradle
 sed -i 's%tv.org.eu.bunnyabc%tv.org.eu.bunnyabc.mla%g' app/build.gradle
 sed -i 's%Jade%VIP影视%g' app/src/main/res/values/strings.xml
