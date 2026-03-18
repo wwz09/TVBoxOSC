273727f7e75e275e0da834e7bceb579b1ca5a209
wwz09 <47264059+wwz09@users.noreply.github.com>
2026-03-18 14:20:44 +0800

    Update DCDIYA.sh

--- DIY/sh/DCDIYA.sh
+++ DIY/sh/DCDIYA.sh
@@ -4,10 +4,10 @@
 sed -i 's%https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json%https://gitea.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%http://www.饭太硬.net/tv/%https://gitea.wwz09.xin:5555/wwz09/ubuntu/raw/main/main.json%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
 sed -i 's%http://www.饭太硬.net/tv%https://gitea.wwz09.xin:5555/wwz09/ubuntu/raw/main/live2.txt%g' app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
-sed -i 's%(Hawk.get(HawkConfig.API_URL, ""))%(Hawk.get(HawkConfig.API_URL, "https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%url + URL.DEFAULT_STORE_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%url + URL.DEFAULT_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
-sed -i 's%(HawkConfig.LIVE_URL, ""))%(HawkConfig.LIVE_URL, "https://git.wwz09.xin:5555/wwz09/IPTV-tv/raw/branch/main/merged_output.txt"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%(Hawk.get(HawkConfig.API_URL, ""))%(Hawk.get(HawkConfig.API_URL, "https://gitea.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%url + URL.DEFAULT_STORE_API_URL%"https://gitea.wwz09.xin:5555/wwz09/ubuntu/raw/main/TXT/dc.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%url + URL.DEFAULT_API_URL%"https://gitea.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/main.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
+sed -i 's%(HawkConfig.LIVE_URL, ""))%(HawkConfig.LIVE_URL, "https://gitea.wwz09.xin:5555/wwz09/IPTV-tv/raw/branch/main/merged_output.txt"))%g' app/src/main/java/com/github/tvbox/osc/bbox/ui/dialog/ApiDialog.java
 # sed -i 's%URL.DEFAULT_STORE_API_URL%"https://git.wwz09.xin:5555/wwz09/ubuntu/raw/branch/main/TXT/dc.json"%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 # sed -i 's%// String defaultApiName%String defaultApiName%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
 # sed -i 's%// String defaultApi%String defaultApi%g' app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
